47bb4e7057c87d411fbb38601e721a94f62cd706
U-UMNGANEWAMI\altac <altac-lancing@hotmail.com>
2023-06-18 09:31:41 +1000

    whoops! forgot to actually edit the if statement to check for the makefile!

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ echo Exporting haxe project
 haxe build.hxml
 echo Haxe export done!
 echo Compiling exported haxe code to 3ds rom!
-if exist yourfilename (
+if exist export/Makefile (
     echo Makefile found!
 ) else (
     echo Makefile not found, adding it in
