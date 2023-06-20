f25b3261576b1c3a87746b2f3e7a86c2644f80a6
U-UMNGANEWAMI\altac <altac-lancing@hotmail.com>
2023-06-20 23:47:16 +1000

    better look for build.bat plus error checking

--- build.bat
+++ build.bat
@@ -1,15 +1,41 @@
 @echo off
-echo Exporting haxe project
-haxe build.hxml
-echo Haxe export done!
-echo Compiling exported haxe code to 3ds rom!
-if exist export/Makefile (
-    echo Makefile found!
-) else (
-    echo Makefile not found, adding it in
-    xcopy /E "./assets" "./export" 
+if exist export (
+    echo --------------------------
+    echo Deleting the export folder.
+    RMDIR "export" /S /Q
+    mkdir "export"
 )
-cd export
-make
-cd .. 
-echo Done!
+echo --------------------------
+echo Exporting haxe code.
+haxe build.hxml
+if errorlevel 1 (
+    echo UNSUCCESSFUL HAXE COMPILE!
+    echo check above for error!
+    echo
+    echo deleting the export folder!
+    RMDIR "export" /S /Q
+    echo --------------------------
+) else (
+    echo Haxe export successful!
+    
+    echo --------------------------
+    echo Copying game assets
+    xcopy /E "./assets" "./export" 
+    echo --------------------------
+
+    echo Compiling exported haxe code to 3ds rom!
+    echo
+    cd export
+    make
+    cd ..
+    if errorlevel 1 (
+        echo Make error! check above code for the error
+        echo
+        echo deleting the export folder!
+        RMDIR "export" /S /Q
+        echo --------------------------
+    ) else (
+        echo Done!
+        echo --------------------------
+    )
+)
--- cleanbuild.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Deleting the export folder
-RMDIR "export" /S /Q
-mkdir "export"
-./build
